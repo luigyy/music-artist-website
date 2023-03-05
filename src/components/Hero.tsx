866078c737f5176df6456436b6e9ddead8b9af93
luigyy <luigy.valverde@ucr.ac.cr>
2023-03-04 21:05:21 -0600

    added underline animations

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,20 +3,26 @@ import "./Hero.css";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="absolute text-white flex w-full justify-between font-anton md:px-20 md:pt-5 overflow-hidden">
-      <h1 className={`appear text-2xl`} data-appear="left">
+    <div className="absolute z-50 text-white flex w-full justify-between font-anton md:px-20 md:pt-5 overflow-hidden">
+      <button
+        className={`appear pb-1 text-2xl transition-all underline-animation relative`}
+        data-appear="left"
+      >
         Ske<span className={`text-[#e70024]  `}>ptic </span>
-      </h1>
+      </button>
       <div className="text-lg flex gap-5">
-        <span className={` inline-block appear `} data-appear="right">
+        <button
+          className={`underline-animation inline-block appear `}
+          data-appear="right"
+        >
           Upcoming Gigs
-        </span>
-        <span className={` appear `} data-appear="right">
+        </button>
+        <button className={`underline-animation appear `} data-appear="right">
           Merch
-        </span>
-        <span className={` appear `} data-appear="right">
+        </button>
+        <button className={`underline-animation appear `} data-appear="right">
           Menu
-        </span>
+        </button>
       </div>
     </div>
   );
@@ -36,20 +42,26 @@ const page2: React.FC<page2Props> = ({}) => {
       </div>
       <Navbar />
 
-      <div className="absolute flex gap-7 top-1/2 -rotate-90 -translate-y-1/2  ">
+      <div className="absolute flex gap-7 top-1/2 z-50 -rotate-90 -translate-y-1/2  ">
         <h1
-          className="text-zinc-200 text-[12px] font-anton w-24 appear"
+          className="text-zinc-200 underline-animation  text-[12px] font-anton w-24 appear"
           data-appear="left"
         >
           Nigerian rapper, music and record producer
         </h1>
         <h1
-          className="text-zinc-200 text-[12px] font-anton w-24  appear "
+          className="text-zinc-200 underline-animation pb-2 text-[12px] font-anton w-24  appear "
           data-appear="left"
         >
           Also known as founder of the nigerian rap movement
         </h1>
       </div>
+
+      {/* <div className="absolute font-anton">
+        <h1 className="text-zinc-200">Latest hits </h1>
+        <div></div>
+        <div></div>
+      </div> */}
       {/* initial text  */}
       <div className="w-full h-screen flex justify-center items-center absolute overflow-hidden ">
         <span
